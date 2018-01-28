f19c3d0a4d57f8dae9f1892b78c89defa3836b52
tbess <timothy.bess@ignitionone.com>
2018-01-27 22:20:32 -0500

    * format some things better * fix some opcode bugs * add debug messages for testing

--- src/gameboy/cpu.rs
+++ src/gameboy/cpu.rs
@@ -30,6 +30,7 @@ impl Cpu {
             memory.executed_bios();
         }
         let opcode = self.read_u8_at_pc(memory);
+        println!("opcode: {:#x}", opcode);
         match opcode {
             0x31 => { // LD sp, nn
                 self.sp = self.read_u16_at_pc(memory);
@@ -53,6 +54,7 @@ impl Cpu {
             }
             0xcb => { // Special multibyte instructions
                 let special_op = self.read_u8_at_pc(memory);
+                println!("Special opcode: {:#x}", special_op);
                 match special_op {
                     0x40 ... 0x7f => { // BIT b, r operations
                         let bit_to_check = (special_op - 0x40) / 0x08;
@@ -65,6 +67,7 @@ impl Cpu {
                 }
             }
             0x20 | 0x28 | 0x30 | 0x38 => { // JR cc, n
+                let offset = self.read_u8_at_pc(memory) as i8;
                 match (opcode >> 3) & 0x03 {
                     0x0 => { // NZ
                         if self.flags.zero { return; }
@@ -80,202 +83,75 @@ impl Cpu {
                     }
                     _ => { panic!("Invalid Jump opcode: {:#x}", opcode); }
                 }
-                let offset = self.read_u8_at_pc(memory);
-                self.pc += offset as u16;
-            }
-            0x40 => { // LD B, B
-                self.bc.hi = self.bc.hi;
-            }
-            0x41 => { // LD B, C
-                self.bc.hi = self.bc.lo;
-            }
-            0x42 => { // LD B, D
-                self.bc.hi = self.de.hi;
-            }
-            0x43 => { // LD B, E
-                self.bc.hi = self.de.lo;
-            }
-            0x44 => { // LD B, H
-                self.bc.hi = self.hl.hi;
-            }
-            0x45 => { // LD B, L
-                self.bc.hi = self.hl.lo;
-            }
-            0x46 => { // LD B, (HL)
-                self.bc.hi = memory.read_u8(self.hl.get_combined());
-            }
-            0x47 => { // LD B, A
-                self.bc.hi = self.af.hi;
-            }
-            0x48 => { // LD C, B
-                self.bc.lo = self.bc.hi;
-            }
-            0x49 => { // LD C, C
-                self.bc.lo = self.bc.lo;
-            }
-            0x4a => { // LD C, D
-                self.bc.lo = self.de.hi;
-            }
-            0x4b => { // LD C, E
-                self.bc.lo = self.de.lo;
-            }
-            0x4c => { // LD C, H
-                self.bc.lo = self.hl.hi;
-            }
-            0x4d => { // LD C, L
-                self.bc.lo = self.hl.lo;
-            }
-            0x4e => { // LD C, (HL)
-                self.bc.lo = memory.read_u8(self.hl.get_combined());
-            }
-            0x4f => { // LD C, A
-                self.bc.lo = self.af.hi;
-            }
-            0x50 => { // LD D, B
-                self.de.hi = self.bc.hi;
-            }
-            0x51 => { // LD D, C
-                self.de.hi = self.bc.lo;
-            }
-            0x52 => { // LD D, D
-                self.de.hi = self.de.hi;
-            }
-            0x53 => { // LD D, E
-                self.de.hi = self.de.lo;
-            }
-            0x54 => { // LD D, H
-                self.de.hi = self.hl.hi;
-            }
-            0x55 => { // LD D, L
-                self.de.hi = self.hl.lo;
-            }
-            0x56 => { // LD D, (HL)
-                self.de.hi = memory.read_u8(self.hl.get_combined());
-            }
-            0x57 => { // LD D, A
-                self.de.hi = self.af.hi;
-            }
-            0x58 => { // LD E, B
-                self.de.lo = self.bc.hi;
-            }
-            0x59 => { // LD E, C
-                self.de.lo = self.bc.lo;
-            }
-            0x5a => { // LD E, D
-                self.de.lo = self.de.hi;
-            }
-            0x5b => { // LD E, E
-                self.de.lo = self.de.lo;
-            }
-            0x5c => { // LD E, H
-                self.de.lo = self.hl.hi;
-            }
-            0x5d => { // LD E, L
-                self.de.lo = self.hl.lo;
-            }
-            0x5e => { // LD E, (HL)
-                self.de.lo = memory.read_u8(self.hl.get_combined());
-            }
-            0x5f => { // LD E, A
-                self.de.lo = self.af.hi;
-            }
-            0x60 => { // LD H, B
-                self.hl.hi = self.bc.hi;
-            }
-            0x61 => { // LD H, C
-                self.hl.hi = self.bc.lo;
-            }
-            0x62 => { // LD H, D
-                self.hl.hi = self.de.hi;
-            }
-            0x63 => { // LD H, E
-                self.hl.hi = self.de.lo;
-            }
-            0x64 => { // LD H, H
-                self.hl.hi = self.hl.hi;
-            }
-            0x65 => { // LD H, L
-                self.hl.hi = self.hl.lo;
-            }
-            0x66 => { // LD H, (HL)
-                self.hl.hi = memory.read_u8(self.hl.get_combined());
-            }
-            0x67 => { // LD H, A
-                self.hl.hi = self.af.hi;
-            }
-            0x68 => { // LD L, B
-                self.hl.lo = self.bc.hi;
-            }
-            0x69 => { // LD L, C
-                self.hl.lo = self.bc.hi;
-            }
-            0x6a => { // LD L, D
-                self.hl.lo = self.de.hi;
-            }
-            0x6b => { // LD L, E
-                self.hl.lo = self.de.lo;
-            }
-            0x6c => { // LD L, H
-                self.hl.lo = self.hl.hi;
-            }
-            0x6d => { // LD L, L
-                self.hl.lo = self.hl.lo;
-            }
-            0x6e => { // LD L, (HL)
-                self.hl.lo = memory.read_u8(self.hl.get_combined());
-            }
-            0x6f => { // LD L, A
-                self.hl.lo = self.af.hi;
-            }
-            0x70 => { // LD (HL), B
-                memory.write_u8(self.hl.get_combined(), self.bc.hi);
-            }
-            0x71 => { // LD (HL), C
-                memory.write_u8(self.hl.get_combined(), self.bc.lo);
-            }
-            0x72 => { // LD (HL), D
-                memory.write_u8(self.hl.get_combined(), self.de.hi);
-            }
-            0x73 => { // LD (HL), E
-                memory.write_u8(self.hl.get_combined(), self.de.lo);
-            }
-            0x74 => { // LD (HL), H
-                memory.write_u8(self.hl.get_combined(), self.hl.hi);
-            }
-            0x75 => { // LD (HL), L
-                memory.write_u8(self.hl.get_combined(), self.hl.lo);
-            }
-            0x76 => { // HALT
-                panic!("HALT not implemented"); // TODO
-            }
-            0x77 => { // LD (HL), A
-                memory.write_u8(self.hl.get_combined(), self.af.hi);
-            }
-            0x78 => { // LD A, B
-                self.af.hi = self.bc.hi;
-            }
-            0x79 => { // LD A, C
-                self.af.hi = self.bc.lo;
-            }
-            0x7a => { // LD A, D
-                self.af.hi = self.de.hi;
-            }
-            0x7b => { // LD A, E
-                self.af.hi = self.de.lo;
-            }
-            0x7c => { // LD A, H
-                self.af.hi = self.hl.hi;
-            }
-            0x7d => { // LD A, L
-                self.af.hi = self.hl.lo;
-            }
-            0x7e => { // LD A, (HL)
-                self.af.hi = memory.read_u8(self.hl.get_combined());
-            }
-            0x7f => { // LD A, A
-                self.af.hi = self.af.hi;
-            }
+                println!("Jumping by offset: '{:#x}'", offset);
+                self.pc = (self.pc as i16 + offset as i16) as u16;
+            }
+            0x40 => { self.bc.hi = self.bc.hi; } // LD B, B
+            0x41 => { self.bc.hi = self.bc.lo; } // LD B, C
+            0x42 => { self.bc.hi = self.de.hi; } // LD B, D
+            0x43 => { self.bc.hi = self.de.lo; } // LD B, E
+            0x44 => { self.bc.hi = self.hl.hi; } // LD B, H
+            0x45 => { self.bc.hi = self.hl.lo; } // LD B, L
+            0x46 => { self.bc.hi = memory.read_u8(self.hl.get_combined()); } // LD B, (HL)
+            0x47 => { self.bc.hi = self.af.hi; } // LD B, A
+            0x48 => { self.bc.lo = self.bc.hi; } // LD C, B
+            0x49 => { self.bc.lo = self.bc.lo; } // LD C, C
+            0x4a => { self.bc.lo = self.de.hi; } // LD C, D
+            0x4b => { self.bc.lo = self.de.lo; } // LD C, E
+            0x4c => { self.bc.lo = self.hl.hi; } // LD C, H
+            0x4d => { self.bc.lo = self.hl.lo; } // LD C, L
+            0x4e => { self.bc.lo = memory.read_u8(self.hl.get_combined()); } // LD C, (HL)
+            0x4f => { self.bc.lo = self.af.hi; } // LD C, A
+            0x50 => { self.de.hi = self.bc.hi; } // LD D, B
+            0x51 => { self.de.hi = self.bc.lo; } // LD D, C
+            0x52 => { self.de.hi = self.de.hi; } // LD D, D
+            0x53 => { self.de.hi = self.de.lo; } // LD D, E
+            0x54 => { self.de.hi = self.hl.hi; } // LD D, H
+            0x55 => { self.de.hi = self.hl.lo; } // LD D, L
+            0x56 => { self.de.hi = memory.read_u8(self.hl.get_combined()); } // LD D, (HL)
+            0x57 => { self.de.hi = self.af.hi; } // LD D, A
+            0x58 => { self.de.lo = self.bc.hi; } // LD E, B
+            0x59 => { self.de.lo = self.bc.lo; } // LD E, C
+            0x5a => { self.de.lo = self.de.hi; } // LD E, D
+            0x5b => { self.de.lo = self.de.lo; } // LD E, E
+            0x5c => { self.de.lo = self.hl.hi; } // LD E, H
+            0x5d => { self.de.lo = self.hl.lo; } // LD E, L
+            0x5e => { self.de.lo = memory.read_u8(self.hl.get_combined()); } // LD E, (HL)
+            0x5f => { self.de.lo = self.af.hi; } // LD E, A
+            0x60 => { self.hl.hi = self.bc.hi; } // LD H, B
+            0x61 => { self.hl.hi = self.bc.lo; } // LD H, C
+            0x62 => { self.hl.hi = self.de.hi; } // LD H, D
+            0x63 => { self.hl.hi = self.de.lo; } // LD H, E
+            0x64 => { self.hl.hi = self.hl.hi; } // LD H, H
+            0x65 => { self.hl.hi = self.hl.lo; } // LD H, L
+            0x66 => { self.hl.hi = memory.read_u8(self.hl.get_combined()); } // LD H, (HL)
+            0x67 => { self.hl.hi = self.af.hi; } // LD H, A
+            0x68 => { self.hl.lo = self.bc.hi; } // LD L, B
+            0x69 => { self.hl.lo = self.bc.hi; } // LD L, C
+            0x6a => { self.hl.lo = self.de.hi; } // LD L, D
+            0x6b => { self.hl.lo = self.de.lo; } // LD L, E
+            0x6c => { self.hl.lo = self.hl.hi; } // LD L, H
+            0x6d => { self.hl.lo = self.hl.lo; } // LD L, L
+            0x6e => { self.hl.lo = memory.read_u8(self.hl.get_combined()); } // LD L, (HL)
+            0x6f => { self.hl.lo = self.af.hi; } // LD L, A
+            0x70 => { memory.write_u8(self.hl.get_combined(), self.bc.hi); } // LD (HL), B
+            0x71 => { memory.write_u8(self.hl.get_combined(), self.bc.lo); } // LD (HL), C
+            0x72 => { memory.write_u8(self.hl.get_combined(), self.de.hi); } // LD (HL), D
+            0x73 => { memory.write_u8(self.hl.get_combined(), self.de.lo); } // LD (HL), E
+            0x74 => { memory.write_u8(self.hl.get_combined(), self.hl.hi); } // LD (HL), H
+            0x75 => { memory.write_u8(self.hl.get_combined(), self.hl.lo); } // LD (HL), L
+            0x76 => { panic!("HALT not implemented"); } // TODO HALT
+            0x77 => { memory.write_u8(self.hl.get_combined(), self.af.hi); } // LD (HL), A
+            0x78 => { self.af.hi = self.bc.hi; } // LD A, B
+            0x79 => { self.af.hi = self.bc.lo; } // LD A, C
+            0x7a => { self.af.hi = self.de.hi; } // LD A, D
+            0x7b => { self.af.hi = self.de.lo; } // LD A, E
+            0x7c => { self.af.hi = self.hl.hi; } // LD A, H
+            0x7d => { self.af.hi = self.hl.lo; } // LD A, L
+            0x7e => { self.af.hi = memory.read_u8(self.hl.get_combined()); } // LD A, (HL)
+            0x7f => { self.af.hi = self.af.hi; } // LD A, A
             0xc4 | 0xcc | 0xd4 | 0xdc => { // CALL cc, nn
+                let jump_to_addr = self.read_u16_at_pc(memory);
                 match (opcode >> 3) & 0x03 {
                     0x0 => { // NZ
                         if self.flags.zero { return; }
@@ -291,8 +167,7 @@ impl Cpu {
                     }
                     _ => { panic!("Invalid Call opcode: {:#x}", opcode); }
                 }
-
-                let jump_to_addr = self.read_u16_at_pc(memory);
+                println!("jumping to: {:#x}", jump_to_addr);
                 self.call(memory, jump_to_addr);
             }
             0xcd => { // CALL nn
